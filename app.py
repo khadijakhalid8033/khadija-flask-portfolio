48636e5bd10ba2b7855de188daba6dd6073021ff
Khadijat Khalid <khadijakhalid128@gmail.com>
2025-06-30 11:23:08 +0100

    Initial commit with Flask app + Render fix

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request, redirect, flash # type: ignore
+
+app = Flask(__name__)
+app.secret_key = "secret"
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    if request.method == "POST":
+        name = request.form["name"]
+        email = request.form["email"]
+        message = request.form["message"]
+        print(f"📩 New message from {name} ({email}): {message}")
+        flash("Thank you! Your message was sent successfully.")
+        return redirect("/contact")
+    return render_template("contact.html")
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=10000)
+
